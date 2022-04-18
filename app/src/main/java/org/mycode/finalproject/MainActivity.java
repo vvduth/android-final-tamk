8628cbfbee8374b613a66fe7bc9f6396828fc752
Duc Thai <ducthai060501@gmail.com>
2022-04-19 01:33:19 +0300

    Home page login

--- app/src/main/java/org/mycode/finalproject/MainActivity.java
+++ app/src/main/java/org/mycode/finalproject/MainActivity.java
@@ -3,12 +3,16 @@ package org.mycode.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button joinNowButton, loginButton  ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        joinNowButton = findViewById(R.id.main_join_now_btn);
+        loginButton = findViewById(R.id.main_login_btn);
     }
 }
