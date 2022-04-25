9f38c633a8c8d5b23449401dcd45ffb4ec2f61c2
Duc Thai <ducthai060501@gmail.com>
2022-04-26 01:03:26 +0300

    User login design

--- app/src/main/java/org/mycode/finalproject/MainActivity.java
+++ app/src/main/java/org/mycode/finalproject/MainActivity.java
@@ -2,7 +2,9 @@ package org.mycode.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +16,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         joinNowButton = findViewById(R.id.main_join_now_btn);
         loginButton = findViewById(R.id.main_login_btn);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, loginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
